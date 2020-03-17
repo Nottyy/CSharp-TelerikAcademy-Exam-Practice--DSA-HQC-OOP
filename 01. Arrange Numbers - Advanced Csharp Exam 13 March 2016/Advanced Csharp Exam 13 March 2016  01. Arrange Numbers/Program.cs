d5898c4630f1f677ea7a235334a26e3920159dc7
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-03-17 22:19:34 +0800

    First Commit

---
+++ 01. Arrange Numbers - Advanced Csharp Exam 13 March 2016/Advanced Csharp Exam 13 March 2016  01. Arrange Numbers/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advanced_Csharp_Exam_13_March_2016__01._Arrange_Numbers
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string dig = "";
+            List<Word> sortedList1 = new List<Word>();
+            Word newWord = new Word();
+
+            for (int ind = 0; ind < input.Length; ind++)
+            {
+                char inputt = input[ind];
+                if (inputt == ',')
+                {
+                    ind += 1;
+                    CheckWord(dig, newWord);
+                    sortedList1.Add(newWord);
+                    newWord = new Word();
+                    dig = "";
+                }
+                else
+                {
+                    dig += input[ind].ToString();
+                }
+            }
+
+            CheckWord(dig, newWord);
+            sortedList1.Add(newWord);
+            IEnumerable<Word> list = sortedList1.OrderBy(x => x.DigitName);
+
+            string str = "";
+            int paf = 0;
+
+            foreach (var i in list)
+            {
+                if (paf == 0 || paf == list.Count())
+                {
+                    str += i.Digit;
+                }
+                else
+                {
+                    str += ", " + i.Digit;
+                }
+                paf++;
+            }
+
+
+            Console.WriteLine(str);
+        }
+
+        public class Word { 
+            public int Digit { get; set; }
+            public string DigitName { get; set; }
+        }
+
+        static void CheckWord(string dig, Word newWord)
+        {
+            if (dig.Length > 1)
+            {
+                newWord.DigitName = CheckLongerWord(dig);
+                newWord.Digit = Int32.Parse(dig);
+            }
+            else
+            {
+                newWord.Digit = dig[0] - '0';
+                newWord.DigitName = GetDigitString(dig[0] - '0');
+            }
+        }
+
+        static string CheckLongerWord(string dig)
+        {
+            string word = "";
+            for (int i = 0; i < dig.Length; i++)
+            {
+                if (i == 0 || i == dig.Length)
+                {
+                    word += GetDigitString(dig[i] - '0');
+                }
+                else
+                {
+                    word += "-" + GetDigitString(dig[i] - '0');
+                }
+            }
+
+            return word;
+        }
+        
+
+        static string GetDigitString(int digitName)
+        {
+            string stringName = "";
+            switch (digitName)
+            {
+                case 0:
+                    stringName = "zero";
+                    break;
+                case 1:
+                    stringName = "one";
+                    break;
+                case 2:
+                    stringName = "two";
+                    break;
+                case 3:
+                    stringName = "three";
+                    break;
+                case 4:
+                    stringName = "four";
+                    break;
+                case 5:
+                    stringName = "five";
+                    break;
+                case 6:
+                    stringName = "six";
+                    break;
+                case 7:
+                    stringName = "seven";
+                    break;
+                case 8:
+                    stringName = "eight";
+                    break;
+                case 9:
+                    stringName = "nine";
+                    break;
+                default:
+                    break;
+            }
+
+            return stringName;
+        }
+    }
+}
---
+++ 02. Monopoly - Advanced Csharp Exam 13 March 2016/02. Monopoly - Advanced Csharp Exam 13 March 2016/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _02._Monopoly___Advanced_Csharp_Exam_13_March_2016
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] dimensions = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int rows = int.Parse(dimensions[0]);
+            int cols = int.Parse(dimensions[1]);
+            int evenOrOdd = 1;
+            int money = 50;
+            int turns = 0;
+            int hotelsOwned = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                string lineCommands = Console.ReadLine();
+                
+                for (int col = 0; col < cols; col++)
+                {
+                    int index = ((evenOrOdd % 2 != 0) ? col : (cols - col - 1));
+                    switch (lineCommands[index])
+                    {
+                        case 'H':
+                            hotelsOwned++;
+                            Console.WriteLine("Bought a hotel for {0}. Total hotels: {1}.", money, hotelsOwned);
+                            //
+                            money = 0;
+                            break;
+                        case 'J':
+                            Console.WriteLine("Gone to jail at turn {0}.", turns);
+                            turns += 2;
+                            money += 2 * (hotelsOwned * 10);
+                            break;
+                        case 'S':
+                            int columnIndex = row % 2 == 0 ? col : index;
+                            int moneyToSpent = money < ((row + 1) * (columnIndex + 1)) ? money : (row + 1) * (columnIndex + 1);
+                            money -= moneyToSpent;
+                            Console.WriteLine("Spent {0} money at the shop.", moneyToSpent);
+                            break;
+                    }
+                    money += hotelsOwned * 10;
+                    turns++;
+                }
+               
+                evenOrOdd++;
+            }
+
+            Console.WriteLine("Turns {0}", turns);
+            Console.WriteLine("Money {0}", money);
+        }
+    }
+}
