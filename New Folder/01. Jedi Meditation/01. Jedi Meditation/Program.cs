8d38ca708b2cac2824eaddbc2cd7290d0e16572f
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-03-26 23:59:46 +0800

    01. Jedi Meditation - added

---
+++ New Folder/01. Jedi Meditation/01. Jedi Meditation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Jedi_Meditation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
