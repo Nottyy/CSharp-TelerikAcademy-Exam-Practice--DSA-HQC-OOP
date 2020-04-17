521cb3d2df3ef16720aaca651b9cda575281a252
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-04-17 20:06:40 +0800

    - cardRepository added

---
+++ OOP - Players and Monsters - C# OOP Retake Exam - 18 Apr 2019/PlayersAndMonsters/Repositories/CardRepository.cs
@@ -0,0 +1,48 @@
+﻿namespace PlayersAndMonsters.Repositories
+{
+    using PlayersAndMonsters.Models.Cards.Contracts;
+    using PlayersAndMonsters.Repositories.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CardRepository : ICardRepository
+    {
+        private List<ICard> cards;
+        public CardRepository()
+        {
+            this.cards = new List<ICard>();
+        }
+        public int Count => this.Cards.Count();
+
+        public IReadOnlyCollection<ICard> Cards => this.cards;
+
+        public void Add(ICard card)
+        {
+            if (card == null)
+            {
+                throw new ArgumentException("Card cannot be null.");
+            }
+
+            bool ifPlayerExcists = this.cards.Any(x => x.Name == card.Name);
+            if (ifPlayerExcists)
+            {
+                throw new ArgumentException("Card {0} already exists!", card.Name);
+            }
+
+            this.cards.Add(card);
+        }
+
+        public ICard Find(string name)
+        {
+            return this.cards.FirstOrDefault(x => x.Name == name);
+        }
+
+        public bool Remove(ICard card)
+        {
+            bool isRemoved = this.cards.Remove(card);
+
+            return isRemoved;
+        }
+    }
+}
