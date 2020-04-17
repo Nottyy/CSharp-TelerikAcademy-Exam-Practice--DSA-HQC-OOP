c831c807c6f22772c24e78174598f4d6d0607325
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-04-17 21:19:12 +0800

    - cardFactory, playerFactory, manageController added

---
+++ OOP - Players and Monsters - C# OOP Retake Exam - 18 Apr 2019/PlayersAndMonsters/Core/Factories/PlayerFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace PlayersAndMonsters.Core.Factories
+{
+    using PlayersAndMonsters.Core.Factories.Contracts;
+    using PlayersAndMonsters.Models.Players;
+    using PlayersAndMonsters.Models.Players.Contracts;
+    using PlayersAndMonsters.Repositories;
+    using PlayersAndMonsters.Repositories.Contracts;
+
+    public class PlayerFactory : IPlayerFactory
+    {
+        public IPlayer CreatePlayer(string type, string username)
+        {
+            IPlayer player;
+            if (type == "Beginner")
+            {
+                player = new Beginner(new CardRepository(), username);
+            }
+            else
+            {
+                player = new Advanced(new CardRepository(), username);
+            }
+
+            return player;
+        }
+    }
+}
--- OOP - Players and Monsters - C# OOP Retake Exam - 18 Apr 2019/PlayersAndMonsters/Core/ManagerController.cs
+++ OOP - Players and Monsters - C# OOP Retake Exam - 18 Apr 2019/PlayersAndMonsters/Core/ManagerController.cs
@@ -1,38 +1,93 @@
 ﻿namespace PlayersAndMonsters.Core
 {
     using System;
-
+    using System.Text;
     using Contracts;
+    using PlayersAndMonsters.Common;
+    using PlayersAndMonsters.Core.Factories;
+    using PlayersAndMonsters.Core.Factories.Contracts;
+    using PlayersAndMonsters.Models.BattleFields;
+    using PlayersAndMonsters.Models.BattleFields.Contracts;
+    using PlayersAndMonsters.Repositories;
+    using PlayersAndMonsters.Repositories.Contracts;
 
     public class ManagerController : IManagerController
     {
+        ICardFactory cardfactory;
+        IPlayerFactory playerFactory;
+        ICardRepository cardRepository;
+        IPlayerRepository playerRepository;
+        IBattleField battlefield;
         public ManagerController()
         {
+            this.cardfactory = new CardFactory();
+            this.playerFactory = new PlayerFactory();
+            this.cardRepository = new CardRepository();
+            this.playerRepository = new PlayerRepository();
+            this.battlefield = new Battlefield();
         }
 
         public string AddPlayer(string type, string username)
         {
-            throw new NotImplementedException();
+            var player = this.playerFactory.CreatePlayer(type, username);
+            this.playerRepository.Add(player);
+
+            var message = string.Format(ConstantMessages.SuccessfullyAddedPlayer, type, username);
+            return message;
         }
 
         public string AddCard(string type, string name)
         {
-            throw new NotImplementedException();
+            var card = this.cardfactory.CreateCard(type, name);
+            this.cardRepository.Add(card);
+
+            var message = string.Format(ConstantMessages.SuccessfullyAddedCard, type, name);
+            return message;
         }
 
         public string AddPlayerCard(string username, string cardName)
         {
-            throw new NotImplementedException();
+            var card = this.cardRepository.Find(cardName);
+            var player = this.playerRepository.Find(username);
+
+            player.CardRepository.Add(card);
+
+            var message = string.Format(ConstantMessages.SuccessfullyAddedPlayerWithCards, cardName, username);
+
+            return message;
         }
 
         public string Fight(string attackUser, string enemyUser)
         {
-            throw new NotImplementedException();
+            var attackPlayer = this.playerRepository.Find(attackUser);
+            var enemyPlayer = this.playerRepository.Find(enemyUser);
+
+            this.battlefield.Fight(attackPlayer, enemyPlayer);
+
+            var message = string.Format(ConstantMessages.FightInfo, attackPlayer.Health, enemyPlayer.Health);
+
+            return message;
         }
 
         public string Report()
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var player in playerRepository.Players)
+            {
+                var playerMessage = string.Format(ConstantMessages.PlayerReportInfo, player.Username, player.Health, player.CardRepository.Count);
+                sb.AppendLine(playerMessage);
+
+                foreach (var playerCard in player.CardRepository.Cards)
+                {
+                    var cardMessage = string.Format(ConstantMessages.CardReportInfo, playerCard.Name, playerCard.DamagePoints);
+                    sb.AppendLine(cardMessage);
+                }
+
+                sb.AppendLine(ConstantMessages.DefaultReportSeparator);
+            }
+
+            return sb.ToString();
         }
     }
 }
